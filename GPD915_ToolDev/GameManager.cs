ad8a6385357f2034fdf63e194fa51fb19e09b1d4
Kevin Schwarz <k.schwarz@sae.edu>
2016-07-04 14:43:57 +0200

    Unterricht 04.07.

--- GPD915_ToolDev/GameManager.cs
+++ GPD915_ToolDev/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Xml.Linq;
 
 namespace GPD915_ToolDev
@@ -13,8 +14,10 @@ namespace GPD915_ToolDev
     {
         
         // eine liste aller spiele
-        public List<Game> games;
-
+        public List<Game> games = new List<Game>();
+        
+        // Speichert alle Game einstellungen in ein
+        // XML-Dokument
         public void Save(string _saveLoc)
         {
             // xml document erstellen
@@ -32,12 +35,42 @@ namespace GPD915_ToolDev
                 XElement gameElement = new XElement("Game");
                 // und dem Element <GameManager> unterordnen
                 gameManagerElement.Add(gameElement);
-
-
+                // spiel in das Element speichern
+                game.Save(gameElement);
             }
 
             // das document in die _saveLoc datei speichern
             doc.Save(_saveLoc);
+        }
+
+        // Lädt eine Einstellungs-Datei
+        public void Load(string _saveLoc)
+        {
+            try
+            {
+                // reset
+                games.Clear();
+
+                // unser xml document laden
+                XDocument doc = XDocument.Load(_saveLoc);
+                XElement gameManagerElement = doc.Element("GameManager");
+
+                // über alle child-elemente des GameManager iterieren
+                foreach (XElement gameElement in
+                    gameManagerElement.Elements("Game"))
+                {
+                    // das element als spiel einstellung laden
+                    games.Add(new Game(gameElement));
+                }
+            }
+            catch (Exception e)
+            {
+                // fehlermeldung zeigen
+                MessageBox.Show(e.ToString(), "Fehler beim Laden.");
+
+                // resetten
+                games.Clear();
+            }
         }
 
     }
@@ -45,11 +78,49 @@ namespace GPD915_ToolDev
     // Enthält die Einstellungen eines Spieles
     public class Game
     {
+        // der Name des Spieles
+        public string Name { get; set; }
 
         // der speicherort des spieles (null = nicht installiert)
         public string GameDir { get; set; }
         
+        public Game()
+        { }
 
+        public Game(XElement _element)
+        {
+            // das attribut für die GameDir suchen
+            XAttribute gameDirAtt = _element.Attribute("GameDir");
+            // die GameDir auslesen
+            GameDir = (string)gameDirAtt;
+            
+            // ist die länge der GameDir 0
+            if(GameDir.Length == 0)
+            {
+                // GameDir auf null (= nicht installiert) setzten
+                GameDir = null;
+            }
+
+            // das attribut für den namen suchen
+            XAttribute nameAtt = _element.Attribute("Name");
+            // und den namen auslesen
+            Name = (string)nameAtt;
+        }
+
+        // Speichert das Game Objekt in dem XElement
+        public void Save(XElement _element)
+        {
+            // Ein Attribut mit dem Namen "GameDir",
+            // welches die GameDir beinhaltet
+            XAttribute gameDirAtt =
+                new XAttribute("GameDir", GameDir != null ? GameDir : "");
+            _element.Add(gameDirAtt);
+
+            // Ein Attribut mit dem Namen "Name",
+            // welches den Namen beinhaltet
+            XAttribute nameAtt = new XAttribute("Name", Name);
+            _element.Add(nameAtt);
+        }
 
     }
 
--- GPD915_ToolDev/MainWindow.xaml.cs
+++ GPD915_ToolDev/MainWindow.xaml.cs
@@ -21,9 +21,29 @@ namespace GPD915_ToolDev
     public partial class MainWindow : Window
     {
 
+        private GameManager gameManager;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // einstellungen laden
+            gameManager = new GameManager();
+            gameManager.Load("E://Test.xml");
+
+            foreach (Game game in gameManager.games)
+            {
+                // tabitem erstellen
+                TabItem item = new TabItem();
+                // dem tabitem den namen des spieles als namen geben
+                // item.Name = game.Name;
+                // den anzeigenamen des tabs auf den spielenamen setzten
+                item.Header = game.Name;
+                // ein neues gametab erstellen und dem tabitem hinzufügen
+                item.Content = new GameTab();
+                // das tabitem dem tabcontrol hinzufügen
+                GameTabControl.Items.Add(item);
+            }
         }        
 
     }
